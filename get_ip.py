f1b841863087fbe172007f73738502e9191a784a
mayermakes <49558118+mayermakes@users.noreply.github.com>
2019-09-05 09:02:40 +0200

    Update get_ip.py

--- get_ip.py
+++ get_ip.py
@@ -4,7 +4,6 @@ import socket
 def get_ip():
     ip = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     try:
-        # doesn't even have to be reachable
         ip.connect(('10.255.255.255', 1))
         IP = ip.getsockname()[0]
     except:
